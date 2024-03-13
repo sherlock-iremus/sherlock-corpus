27688b1b1e2f3ce52be3d8b5f985a347b9c2c7d3
Thomas ⬡ Bottini <thomas.bottini@gmail.com>
2024-03-13 15:37:18 +0100

    🌴

--- src/components/corpus/TonalitiesCorpuses.tsx
+++ src/components/corpus/TonalitiesCorpuses.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
 import { useGetSparqlQueryResultQuery } from 'sherlock-rdf/lib/rtkquery-service-sparql'
 import { useSearchParams } from 'react-router-dom'
+import { SparqlQueryResultObject } from 'sherlock-rdf/lib/sparql-result'
 
 export default function TonalitiesCorpuses() {
-    const { data, error, isLoading } = useGetSparqlQueryResultQuery(`
+  const { data, error, isLoading } = useGetSparqlQueryResultQuery(`
     SELECT * WHERE {
         GRAPH <http://data-iremus.huma-num.fr/graph/tonalities-projects-and-corpuses> {
           ?s ?p ?o .
@@ -11,11 +12,9 @@ export default function TonalitiesCorpuses() {
       }
     `)
 
-    const _ = data || error || isLoading
+  const _ = data || error || isLoading
 
-    const [searchParams] = useSearchParams();
-
-    return <pre className=' whitespace-break-spaces'>
-        {JSON.stringify(_)}
-    </pre>
+  return <pre className=' whitespace-break-spaces'>
+    {JSON.stringify(_)}
+  </pre>
 }
--- src/components/root/App.tsx
+++ src/components/root/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes, useNavigate } from 'react-router-dom'
 import { NextUIProvider } from '@nextui-org/react'
 import { Listbox, ListboxItem } from '@nextui-org/react'
-import { useState } from 'react'
+import { ReactElement, useState } from 'react'
 import { FaEmpire } from 'react-icons/fa6'
 import { FaPeopleCarryBox } from 'react-icons/fa6'
 import { GiAudioCassette } from 'react-icons/gi'
@@ -20,6 +20,7 @@ import MyCollaborators from '../people/MyCollaborators'
 import DocumentsThatIHaveAnnotated from '../documents/DocumentsThatIHaveAnnotated'
 import MyPersonnalCorpus from '../corpus/MyPersonnalCorpus'
 import Help from '../misc/Help'
+import { IconType } from 'react-icons'
 
 function App() {
     const navigate = useNavigate()
@@ -30,15 +31,24 @@ function App() {
         setSelectedKeys([e])
     }
 
-    const menuData = [
-        ['help', 'Help', <GiDuck />],
-        ['tonalities_corpuses', 'Tonalities corpuses', <PiVinylRecordFill />],
-        ['public_corpuses', 'Public corpuses', <FaEmpire />],
-        ['my_corpuses', 'My corpuses', <GiAudioCassette />],
-        ['my_projects', 'My projects', <GiSherlockHolmes />],
-        ['my_collaborators', 'My collaborators', <FaPeopleCarryBox />],
-        ['documents_that_i_have_annotated', 'Documents that I have annotated', <GiTreeRoots />],
-        ['my_personnal_corpus', 'My personnal corpus', <GiLockedChest />]
+    type MenuData = Array<MenuDatum>
+
+    type MenuDatum = {
+        key: string,
+        label: string,
+        icon: ReactElement,
+        element: ReactElement
+    }
+
+    const menuData: MenuData = [
+        { key: 'help', label: 'Help', icon: <GiDuck />, element: <Help /> },
+        { key: 'tonalities_corpuses', label: 'Tonalities corpuses', icon: <PiVinylRecordFill />, element: <TonalitiesCorpuses /> },
+        { key: 'public_corpuses', label: 'Public corpuses', icon: <FaEmpire />, element: <PublicCorpuses /> },
+        { key: 'my_corpuses', label: 'My corpuses', icon: <GiAudioCassette />, element: <MyCorpuses /> },
+        { key: 'my_projects', label: 'My projects', icon: <GiSherlockHolmes />, element: <MyProjects /> },
+        { key: 'my_collaborators', label: 'My collaborators', icon: <FaPeopleCarryBox />, element: <MyCollaborators /> },
+        { key: 'documents_that_i_have_annotated', label: 'Documents that I have annotated', icon: <GiTreeRoots />, element: <DocumentsThatIHaveAnnotated /> },
+        { key: 'my_personnal_corpus', label: 'My personnal corpus', icon: <GiLockedChest />, element: <MyPersonnalCorpus /> },
     ]
 
     return (
@@ -57,23 +67,16 @@ function App() {
                             onAction={handleAction}
                         >
                             {menuData.map(_ => <ListboxItem
-                                key={String(_[0])}
-                                href={`/${_[0]}`}
-                                startContent={_[2]}
-                            >{_[1]}</ListboxItem>)}
+                                key={String(_.key)}
+                                href={`/${_.key}`}
+                                startContent={_.icon}
+                            >{_.label}</ListboxItem>)}
                         </Listbox>
                     </nav>
                     <main>
                         <Routes>
                             <Route path='/' errorElement={<ErrorPage />}>
-                                <Route path='help' element={<Help />}></Route>
-                                <Route path='tonalities_corpuses' element={<TonalitiesCorpuses />}></Route>
-                                <Route path='public_corpuses' element={<PublicCorpuses />}></Route>
-                                <Route path='my_corpuses' element={<MyCorpuses />}></Route>
-                                <Route path='my_projects' element={<MyProjects />}></Route>
-                                <Route path='my_collaborators' element={<MyCollaborators />}></Route>
-                                <Route path='documents_that_i_have_annotated' element={<DocumentsThatIHaveAnnotated />}></Route>
-                                <Route path='my_personnal_corpus' element={<MyPersonnalCorpus />}></Route>
+                                {menuData.map(_ => <Route key={_.key} path={_.key} element={_.element} />)}
                             </Route>
                         </Routes>
                     </main>
