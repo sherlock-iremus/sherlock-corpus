fb160b55ef0334f32d9c1597d3c1ecab6ec788f4
Thomas ⬡ Bottini <thomas.bottini@gmail.com>
2024-03-20 15:27:00 +0100

    🌴

--- src/components/root/App.tsx
+++ src/components/root/App.tsx
@@ -71,7 +71,7 @@ function App() {
                             selectionMode='single'
                             selectedKeys={selectedKeys}
                             onAction={handleAction}
-                            className="p-0 gap-0 divide-y divide-default-300/50 dark:divide-default-100/80 bg-content1 max-w-[300px] overflow-visible shadow-small rounded-medium"
+                            className="p-0 gap-0 shadow-small rounded-medium"
                             itemClasses={{
                                 base: "px-3 first:rounded-t-medium last:rounded-b-medium rounded-none gap-3 h-12 data-[hover=true]:bg-default-100/80",
                             }}
