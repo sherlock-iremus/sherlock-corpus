074c195b32de8b54158bcb78a993c174aa72638e
Félix <felix.poullet-pages@cnrs.fr>
2024-04-09 17:27:16 +0200

    save tonaliteam id as constant

--- src/components/corpus/TonalitiesCorpuses.tsx
+++ src/components/corpus/TonalitiesCorpuses.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react'
 import { Card, CardBody, Spinner } from '@nextui-org/react'
-import { useGetSparqlQueryResultQuery } from 'sherlock-rdf/lib/rtkquery-service-sparql'
-import { SparqlQueryResultObject_Binding } from 'sherlock-rdf/lib/sparql-result'
+import { useGetFlattenedSparqlQueryResultQuery } from 'sherlock-rdf/lib/rtkquery-service-sparql'
 import { getCorpusByCreator } from 'sherlock-sparql-queries/lib/corpus'
 import { CorpusView } from './CorpusView'
+import { TONALITEAM, getIri } from '../../utils'
 
 export type Corpus = {
   id: string
@@ -11,24 +11,13 @@ export type Corpus = {
   manifestations: number
 }
 
-const createCorpusFromQuery = (binding: SparqlQueryResultObject_Binding): Corpus =>
-  binding
-    ? {
-        id: binding.corpus.value,
-        label: binding.label.value,
-        manifestations: parseInt(binding.manifestations.value),
-      }
-    : { id: '', label: '', manifestations: 0 }
-
 export default function TonalitiesCorpuses() {
   const [selectedCorpus, setSelectedCorpus] = useState<Corpus | null>(null)
-  const [Corpuses, setCorpuses] = useState<Corpus[]>([])
-  const { data } = useGetSparqlQueryResultQuery(
-    getCorpusByCreator('http://data-iremus.huma-num.fr/id/56ed1334-b47a-440a-b78d-04c8d3cfc311') // TONALITEAM
-  )
+  const [corpus, setCorpuses] = useState<Corpus[]>([])
+  const { data } = useGetFlattenedSparqlQueryResultQuery(getCorpusByCreator(getIri(TONALITEAM)))
 
   useEffect(() => {
-    if (data) setCorpuses(data.results.bindings.map(createCorpusFromQuery))
+    if (data) setCorpuses(data)
   }, [data])
 
   if (!data) return <Spinner />
@@ -36,8 +25,8 @@ export default function TonalitiesCorpuses() {
   return (
     <div className="gap-2 grid grid-cols-2 sm:grid-cols-4 p-4">
       <CorpusView {...{ selectedCorpus, setSelectedCorpus }} />
-      {Corpuses.map(corpus => (
-        <Card shadow="sm" key={corpus.id} isPressable onPress={() => setSelectedCorpus(corpus)}>
+      {corpus.map(corpus => (
+        <Card key={corpus.id} shadow="sm" isPressable onPress={() => setSelectedCorpus(corpus)}>
           <CardBody className="text-small justify-between">
             <b>{corpus.label}</b>
             <p className="text-default-500">{corpus.manifestations} items</p>
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
 export const getIri = (uuid: string) => `http://data-iremus.huma-num.fr/id/${uuid}`
 
 export const getUuid = (iri: string) => iri.split('/').pop()
+
+export const TONALITEAM = '56ed1334-b47a-440a-b78d-04c8d3cfc311'
