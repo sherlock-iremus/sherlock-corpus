8532fda1c86934357c1ea6de000d5bf23aec382d
Félix <felix.poullet-pages@cnrs.fr>
2024-04-17 11:11:57 +0200

    update react router v6 syntax

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import { store } from './store'
 import App from './components/root/App'
 import './index.css'
 
+const router = createBrowserRouter([{ path: '*', element: <App /> }], {
+  basename: '/corpus',
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
